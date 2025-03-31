096166eb387e3a07b368b26b5e800f3b098d57bf
Cristian <cristianfiguerola@activalink.com>
2025-03-31 08:07:13 +0200

    Mantine instalado

--- frontend/inventario/src/App.jsx
+++ frontend/inventario/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Button } from '@mantine/core'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div style={{ padding:20 }}>
+        <Button color="blue">Boton de Mantine</Button>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
