62f05011bfb4d3551c62c59b52d772079f910d7c
CriFigShe <crifigshe@gmail.com>
2025-06-11 14:00:03 +0200

    variables env y cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { validateToken } = require("./middleware/validate-token.js");
 
 ////////////////////////////////////////////////////////////////////////////////////
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 const path = require("path");
 
@@ -20,8 +20,6 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://inventariio.netlify.app"
 ];
-
-app.use(cors());
 
 app.use(cors({
   origin: function (origin, callback) {
